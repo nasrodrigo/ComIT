e4eb381b01cd8de2fe6f7acbd5596cf72474e6fa
Rodrigo Nascimento <nas.rodrigorj@gmail.com>
2020-10-01 21:05:13 -0500

    adding if =(

--- js-exercises/08-switch-statement/index.js
+++ js-exercises/08-switch-statement/index.js
@@ -3,6 +3,24 @@
 let weekday = prompt(`Enter with weekday(Valid value is betwen 1 and 7)`, 100);
 let msg;
 
+if(weekday == 1) {
+    console.log("Today is Monday");
+}else if (weekday == 2) {
+    console.log("Today is Tuesday");
+}else if (weekday == 3) {
+    console.log("Today is Wednesday");
+}else if (weekday == 4) {
+    console.log("Today is Thursday");
+}else if (weekday == 5) {
+    console.log("Today is Friday");
+}else if (weekday == 6) {
+    console.log("Today is Saturday");
+}else if (weekday == 7) {
+    console.log("Today is Sunday");
+}else {
+    console.log(`${weekday} is a invalide value`);
+}
+
 switch (Number(weekday)) {
     case 1:
         msg = "Today is Monday";
