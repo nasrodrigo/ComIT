46a39c2c02605711b44ced9f10cc865285ffc532
Rodrigo Nascimento <nas.rodrigorj@gmail.com>
2020-10-05 20:27:24 -0500

    Converting String to Number before comparing

--- js-exercises/04-js-comparison/index.js
+++ js-exercises/04-js-comparison/index.js
@@ -1,5 +1,5 @@
-"use strict"
+"use strict";
 alert("Enter two numbers to see if the first is greater than the second");
-let firstValue = prompt( "First value", 100)
-let secondValue = prompt( "Second value", 100)
+let firstValue = +prompt( "First value", 100)
+let secondValue = +prompt( "Second value", 100)
 alert(`Result: ${firstValue > secondValue}`)
