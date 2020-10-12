7ddf9c948a5c4710904cda8159d15e9e30915338
Rodrigo Nascimento <nas.rodrigorj@gmail.com>
2020-10-12 12:31:31 -0500

    Converting String to Number before operation

--- js-exercises/05-js-conditionals/index.js
+++ js-exercises/05-js-conditionals/index.js
@@ -1,7 +1,7 @@
 "use strict"
 alert("Enter two numbers to see if the first is greater than the second");
-let firstValue = prompt( "First value", 100)
-let secondValue = prompt( "Second value", 100)
+let firstValue = +prompt( "First value", 100)
+let secondValue = +prompt( "Second value", 100)
 if(Number(firstValue) > Number(secondValue)){
     alert(`Result: First value is greater ${firstValue} > ${secondValue}`)
 }else if(Number(firstValue) == Number(secondValue)){
